e750057e86cc51aa256217d19b43c6a2493d62ac
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-04-15 20:49:23 +0200

    feat: disable safety check

--- diff.py
+++ diff.py
@@ -10,6 +10,9 @@ pipe = StableDiffusionPipeline.from_pretrained(
     torch_dtype=torch.float16,
 ).to(device)
 
-propmt = "a cat sissting by the door"
+pipe.safety_checker = None
+pipe.requires_safety_checker = False
 
-pipe(propmt).images[0].save("cat_sitting.png")
+propmt = "Rabit jumping over the moon"
+
+pipe(propmt).images[0].save("rabit.png")
--- hello.py
+++ hello.py
@@ -2,5 +2,4 @@ from genaibook.core import get_device
 
 device = get_device()
 
-
 print(f"Using device: {device}")
